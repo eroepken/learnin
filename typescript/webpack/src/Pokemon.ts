49d149f0d54a3fe2f219975309219d3c98fa340c
Ellie Strejlau <estrejlau@phase2technology.com>
2019-02-12 17:52:35 -0500

    Fix spacing.

--- typescript/webpack/src/Pokemon.ts
+++ typescript/webpack/src/Pokemon.ts
@@ -1,147 +1,147 @@
 enum Types {
 	'Normal',
-  'Fire',
+	'Fire',
 	'Fighting',
 	'Water',
 	'Flying',
-  'Grass',
+	'Grass',
 	'Poison',
-  'Electric',
+	'Electric',
 	'Ground',
-  'Psychic',
+	'Psychic',
 	'Rock',
-  'Ice',
+	'Ice',
 	'Bug',
-  'Dragon',
+	'Dragon',
 	'Ghost',
-  'Dark',
+	'Dark',
 	'Steel',
-  'Fairy',
+	'Fairy',
 	'???',
 }
 
 type Image = {
-  source: string,
-  path: string;
+	source: string,
+	path: string;
 }
 
 abstract class Pokemon {
-  pokedexNum: number;
-  _images: Image[];
-  name?: string;
-  level: number;
-  height: number;
-  weight: number;
-  _types: Types[];
+	pokedexNum: number;
+	_images: Image[];
+	name?: string;
+	level: number;
+	height: number;
+	weight: number;
+	_types: Types[];
 
-  constructor(level: number) {
-    this.level = level;
-  }
+	constructor(level: number) {
+		this.level = level;
+	}
 
-  get images(): string {
-    return this._images.map((v, i) => {
-      return `<img src="${v.path}" title="${this.constructor.name}, Source: ${v.source}" />`;
-    }).join('');
-  }
+	get images(): string {
+		return this._images.map((v, i) => {
+		return `<img src="${v.path}" title="${this.constructor.name}, Source: ${v.source}" />`;
+		}).join('');
+	}
 
-  get types(): string {
-    return this._types.map((v) => {
-      return Types[v];
-    }).join(', ');
-  }
+	get types(): string {
+		return this._types.map((v) => {
+		return Types[v];
+		}).join(', ');
+	}
 
-  get stats(): string {
-    let stats = '';
+	get stats(): string {
+		let stats = '';
 
-    stats = `<ul>
-      <li>Pokedex Number: ${this.pokedexNum}</li>`;
+		stats = `<ul>
+			<li>Pokedex Number: ${this.pokedexNum}</li>`;
 
-    if (this.name != null) {
-      stats += `<li>Name: ${this.name}</li>`;
-    }
+		if (this.name != null) {
+			stats += `<li>Name: ${this.name}</li>`;
+		}
 
-    stats += `<li>Level: ${this.level}</li>
-      <li>Types: ${this.types}</li>
-    </ul>`;
+		stats += `<li>Level: ${this.level}</li>
+			<li>Types: ${this.types}</li>
+			</ul>`;
 
-    return stats;
-  }
+		return stats;
+	}
 
-  changeName = (name): void => {
-    this.name = name;
-  }
+	changeName = (name): void => {
+		this.name = name;
+	}
 
-  render = (): string => `
-    <div class="pokemon">
-      ${this.images}
-      <div class="stats">
-        ${this.stats}
-      </div>
-    </div>
-  `
+	render = (): string => `
+		<div class="pokemon">
+		${this.images}
+		<div class="stats">
+			${this.stats}
+		</div>
+		</div>
+	`
 }
 
 class Bulbasaur extends Pokemon {
 	pokedexNum = 1;
-  _images = [
-    { source: 'Bulbapedia', path: 'https://cdn.bulbagarden.net/upload/thumb/2/21/001Bulbasaur.png/250px-001Bulbasaur.png' }
-  ];
-  _types = [Types.Grass, Types.Poison];
+	_images = [
+		{ source: 'Bulbapedia', path: 'https://cdn.bulbagarden.net/upload/thumb/2/21/001Bulbasaur.png/250px-001Bulbasaur.png' }
+	];
+	_types = [Types.Grass, Types.Poison];
 }
 
 class Pikachu extends Pokemon {
 	pokedexNum = 25;
-  _images = [
-    { source: 'Bulbapedia', path: 'https://cdn.bulbagarden.net/upload/thumb/0/0d/025Pikachu.png/250px-025Pikachu.png' },
-  ];
-  _types = [Types.Electric];
+	_images = [
+		{ source: 'Bulbapedia', path: 'https://cdn.bulbagarden.net/upload/thumb/0/0d/025Pikachu.png/250px-025Pikachu.png' },
+	];
+	_types = [Types.Electric];
 }
 
 class Meowth extends Pokemon {
 	pokedexNum = 52;
-  _images = [
-    { source: 'Bulbapedia', path: 'https://cdn.bulbagarden.net/upload/thumb/d/d6/052Meowth.png/250px-052Meowth.png' }
-  ];
-  _types = [Types.Normal];
+	_images = [
+		{ source: 'Bulbapedia', path: 'https://cdn.bulbagarden.net/upload/thumb/d/d6/052Meowth.png/250px-052Meowth.png' }
+	];
+	_types = [Types.Normal];
 }
 
 class Trainer {
-  _images: Image[] = [];
-  name: string;
-  _party: Array<Pokemon> = [];
+	_images: Image[] = [];
+	name: string;
+	_party: Array<Pokemon> = [];
 
-  constructor(name: string) {
-    this.name = name;
-  }
+	constructor(name: string) {
+		this.name = name;
+	}
 
-  set image(image: Image) {
-    this._images.push(image);
-  }
+	set image(image: Image) {
+		this._images.push(image);
+	}
 
-  get images(): string {
-    return this._images.map((v, i) => {
-      return `<img src="${v.path}" title="${this.constructor.name}, Source: ${v.source}" />`;
-    }).join('');
-  }
+	get images(): string {
+		return this._images.map((v, i) => {
+			return `<img src="${v.path}" title="${this.constructor.name}, Source: ${v.source}" />`;
+		}).join('');
+	}
 
-  addPokemonToParty = (pokemon: Pokemon[]): void => {
-    this._party = [...this._party, ...pokemon];
-  }
+	addPokemonToParty = (pokemon: Pokemon[]): void => {
+		this._party = [...this._party, ...pokemon];
+	}
 
-  get party(): string {
-    return `
-    <div id="party">
-      ${this._party.map((v) => {
-        return v.render();
-      }).join('')}
-    </div>`;
-  }
+	get party(): string {
+		return `
+			<div id="party">
+				${this._party.map((v) => {
+					return v.render();
+				}).join('')}
+			</div>`;
+	}
 
-  render = (): string => `
-    ${this.images}
-    ${this.name}
-    ${this.party}
-  `;
+	render = (): string => `
+		${this.images}
+		${this.name}
+		${this.party}
+	`;
 }
 
 const ellie: Trainer = new Trainer('Ellie');
